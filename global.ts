0cdf5eaea6cc7c7ac76962557350af7ceae8c4d7
Nam Chu Hoai <nambrot@googlemail.com>
2019-08-14 14:25:48 -0700

    initial commit

---
+++ global.ts
@@ -0,0 +1,20 @@
+// TODO remove, move to celo/utils
+export interface Global {
+    btoa: any
+    self: any
+    Buffer: any
+    process: any
+  }
+
+declare var global: Global
+if (typeof global.self === 'undefined') {
+global.self = global
+}
+if (typeof btoa === 'undefined') {
+    global.btoa = function (str) {
+        return new Buffer(str, 'binary').toString('base64');
+    };
+}
+
+global.Buffer = require('buffer').Buffer;
+global.process = require('process');
