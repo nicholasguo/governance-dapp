15b1e0b9519bb2854ed6d1dfbc08919c60960b14
Brice Wang <brice.l.wang@gmail.com>
2019-08-15 13:35:13 -0700

    Spacing change

--- components/Bonds/BondCard.js
+++ components/Bonds/BondCard.js
@@ -2,23 +2,22 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 export default class BondCard extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>
-                    BondCard
-                </Text>
-            </View>
-        )
-    };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          BondCard
+        </Text>
+      </View>
+    )
+  };
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#fff',
-        borderWidth: 1,
-    },
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+  },
 });
-  
