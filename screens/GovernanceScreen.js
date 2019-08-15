8fa5361a3928e568ae2a286738a434b0a27dcae2
Nicholas Guo <nguo@celo.org>
2019-08-15 14:53:52 -0700

    change screens to components

--- screens/GovernanceScreen.js
+++ screens/GovernanceScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ExpoConfigView } from '@expo/samples';
 
-export default function GovernanceScreen() {
+export default class GovernanceScreen extends React.Component {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  render() {
+    return <ExpoConfigView />;
+  }
 }
 
 GovernanceScreen.navigationOptions = {
--- screens/ValidatorsScreen.js
+++ screens/ValidatorsScreen.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 
-export default function ValidatorsScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+export default class ValidatorsScreen extends React.Component {
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/**
+        * Go ahead and delete ExpoLinksView and replace it with your content;
+        * we just wanted to provide you with some helpful links.
+        */}
+        <ExpoLinksView />
+      </ScrollView>
+    );
+  }
 }
 
 ValidatorsScreen.navigationOptions = {
